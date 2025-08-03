b2962310d35b460fa5fb9f9fefd1348f5b275a4c
Odero2002 <oderoemmanuel3@gmail.com>
2025-08-03 14:30:06 +0300

    app

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/vite.svg" className="App-logo" alt="logo" />
+        <h1>Welcome to My React App</h1>
+        <p>
+          This is a simple React application created using Vite.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
