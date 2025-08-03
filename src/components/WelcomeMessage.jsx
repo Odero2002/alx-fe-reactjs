a67bc738d58ee8c37317b87b3f81880469046612
Odero2002 <oderoemmanuel3@gmail.com>
2025-08-03 15:40:03 +0300

    Update WelcomeMessage.jsx

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,11 +1,11 @@
-function WelcomeMessage() {
+import Wenpm run devcd c:\Users\ODERO EMMANUEL\Desktop\LocalRepoProject\alx-fe-reactjslcomeMessage from './components/WelcomeMessage';
+
+function App() {
     return (
         <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
+            <WelcomeMessage />
         </div>
     );
 }
 
-export default WelcomeMessage;
+export default App;
