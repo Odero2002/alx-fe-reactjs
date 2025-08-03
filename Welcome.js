83e79a141357330d49d6257c9f55ed14dbebb796
Odero2002 <oderoemmanuel3@gmail.com>
2025-08-03 14:35:19 +0300

    files contents

---
+++ Welcome.js
@@ -0,0 +1,11 @@
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />src/components/WelcomeMessage.jsx
+        </div>
+    );
+}
+
+export default App;
