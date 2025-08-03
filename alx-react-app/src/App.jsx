615c07a1252955d26dbdd5e338dd4a375c4c0e82
Odero2002 <oderoemmanuel3@gmail.com>
2025-08-03 19:33:50 +0300

    contents

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,18 +3,11 @@ import './App.css';
 import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/vite.svg" className="App-logo" alt="logo" />
-        <h1>Welcome to My React App</h1>
-        <p>
-          This is a simple React application created using Vite.
-        </p>
-      </header>
-      <WelcomeMessage />
-    </div>
-  );
+    return (
+        <div>
+            <WelcomeMessage />
+        </div>
+    );
 }
 
 export default App;
