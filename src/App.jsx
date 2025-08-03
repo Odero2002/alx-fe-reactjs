ddce7540e866f2f3cde0c2fc9145f73a9f043f66
Odero2002 <oderoemmanuel3@gmail.com>
2025-08-03 15:32:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import WelcomeMessage from './components/WelcomeMessage';
+import './App.css';
+import './index.css';
 
 function App() {
     return (
