af62d6a9255e89dd3affbac7bae2411da7eb76f9
Odero2002 <oderoemmanuel3@gmail.com>
2025-08-06 00:34:45 +0300

    Update MainContent.jsx

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+        <main style={{ backgroundColor: '#f9f9f9', padding: '20px', textAlign: 'center' }}>
+            <p style={{ fontSize: '18px', color: '#333' }}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
