78445eb511fa8b0138452018c98ce096d943cad7
Odero2002 <oderoemmanuel3@gmail.com>
2025-08-06 10:10:59 +0300

    files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import MainContent from './components/MainContent';
 
 function App() {
     return (
