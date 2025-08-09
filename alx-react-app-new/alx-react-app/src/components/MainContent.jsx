733a487ef1b1ed3ffc673fd9f780818706cc3683
Odero2002 <oderoemmanuel3@gmail.com>
2025-08-10 00:18:31 +0300

    Update MainContent.jsx

--- alx-react-app-new/alx-react-app/src/components/MainContent.jsx
+++ alx-react-app-new/alx-react-app/src/components/MainContent.jsx
@@ -6,7 +6,9 @@ const MainContent = (style) => {
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
       <h2 style={{ color: 'green' }}>Welcome to My Favorite Cities</h2>
       <p style={{ fontSize: '16px' }}>Here are some of the cities I love:</p>
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
+      <p style={{ fontStyle: 'italic' }}>I love to visit New York, Paris, and Tokyo.</p>
+    
+     <ul style={{ listStyleType: 'none', padding: 0 }}>
         <li style={{ padding: '8px', backgroundColor: 'white', margin: '5px 0' }}>New York</li>
         <li style={{ padding: '8px', backgroundColor: 'white', margin: '5px 0' }}>Paris</li>
         <li style={{ padding: '8px', backgroundColor: 'white', margin: '5px 0' }}>Tokyo</li>
