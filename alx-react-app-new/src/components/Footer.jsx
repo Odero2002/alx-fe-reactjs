63cc9e8c9dbb945c7db95256f34430d8aecf97e0
Odero2002 <oderoemmanuel3@gmail.com>
2025-09-06 01:13:39 +0300

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer>
+        <footer style={{ backgroundColor: 'gray', color: 'white', textAlign: 'center', padding: '10px' }}>
             <p>© 2023 City Lovers</p>
         </footer>
     );
