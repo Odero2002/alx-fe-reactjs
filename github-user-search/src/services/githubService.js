ed5cc444da7b806d0640903f3d7534f610dc6b11
Odero2002 <oderoemmanuel3@gmail.com>
2025-09-07 22:58:54 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,3 +1,11 @@
+import axios from 'axios';
+
+// Fetch user data from GitHub API
+export async function fetchUserData(username) {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+}
+
 export const searchUsers = async ({ username, location, minRepos }) => {
   let query = username;
   if (location) query += ` location:${location}`;
