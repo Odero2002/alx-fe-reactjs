c1dc96c7909f7748d7e0b14c9263eab3078d9a6e
Odero2002 <oderoemmanuel3@gmail.com>
2025-09-10 20:34:25 +0300

    more files

--- react-todo/src/setupTests.js
+++ react-todo/src/setupTests.js
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 import { expect, afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
+import 'jsdom-global/register'; // Explicitly import jsdom-global
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
